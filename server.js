d9e392824e20921cec802833b1cc5d62f94f2512
LNDHULK34 <67345382+LNDHULK34@users.noreply.github.com>
2020-09-13 03:19:23 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+
+const Discord = require("discord.js"); ///crate by mrfix
+const robot_hama = new Discord.Client(); ///crate by mrfix
+
+
+robot_hama.on("ready", () => {
+  robot_hama.user.setActivity("بۆ ڕێکلام کردن لینکی سێرڤەر بنێرە", {
+    type: "PLAYING" 
+  }); 
+  robot_hama.user.setStatus("ONLINE");
+});
+
+
+
+robot_hama.on("message", message => {
+  if (message.content.includes("discord.gg")) {
+        if (message.channel.type === "dm") 
+    {
+      robot_hama.channels.get("753382973507764356").send(
+
+          `**ڕیکلام کرا**
+<@${message.author.id}> `+`${message.content}`
+
+        );
+
+    } 
+
+  }
+
+}); 
+
+robot_hama.login("NzU0NDk1OTQ1NTA4Mzg5MDM0.X11lGw.e4hXUFFzzi1EVhmg0h287nVxkkk");
+
